3d6c5b0fc0717708ac2fe6e153af6f6a41c6ad64
Adilhameed <161675441+adil310@users.noreply.github.com>
2024-03-06 21:10:05 +0500

    Add files via upload

---
+++ pythonList.py
@@ -0,0 +1,21 @@
+         #append Method
+list = ["apple","kiwi","orange"]
+list.append("banana")
+print(list)
+        #clear Method
+fruitlist = ["apple","banana","kiwi"]
+fruitlist.clear()
+print(fruitlist)
+       #extend method
+fruitlist = ["apple","banana","kiwi"]
+car = ["bmw","toyota","farari"]
+fruitlist.extend(car)
+print(fruitlist)
+       #index method
+fruitlist = ["apple","charry","banana"]
+x = fruitlist.index("apple")
+print(x)
+       #insert method
+fruitlist = ["apple","charry","banana"]
+fruitlist.insert(2,"kiwi")
+print(fruitlist)
